0142d69d1a9e49c6acb35fa0d5397fb3e5ee6446
Samartha Bhat <samarthabhat11@gmail.com>
2025-10-09 19:19:23 +0530

    merge-two-list

---
+++ Linked List/mergeTwoLists.go
@@ -0,0 +1,35 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	current := dummy
+	for list1 != nil && list2 != nil{
+		if list1.Val < list2.Val {
+			current.Next = list1
+			list1 = list1.Next
+		} else {
+			current.Next = list2
+			list2 = list2.Next
+		}
+		current = current.Next
+	}
+
+	if list1 != nil {
+		current.Next = list1
+	}else{
+		current.Next = list2
+	}
+	
+	return dummy.Next
+}
+
+func main(){
+	list1 := &ListNode{1, &ListNode{2, &ListNode{4, nil}}}
+	list2 := &ListNode{1, &ListNode{3, &ListNode{4, nil}}}
+	mergeTwoLists(list1, list2)
+}
